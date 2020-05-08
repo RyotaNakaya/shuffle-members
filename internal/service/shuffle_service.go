c6c280b3f6d349940d7edcc75dfde05d9a9fe5e6
Ryota Nakaya <0709nakaya@gmail.com>
2020-05-08 21:40:27 +0900

    シャッフル時にタグを考慮

--- internal/service/shuffle_service.go
+++ internal/service/shuffle_service.go
@@ -19,8 +19,8 @@ func (s *ShuffleService) Shuffle(pid, gcount, mcount int) ([]model.ShuffleLogDet
 	db := db.GetDB()
 
 	// そのプロジェクトに紐付くMemberの一覧を取得する
-	var members []model.Member
-	if err := db.Where("project_id = ?", pid).Preload("ShuffleLogDetail").Find(&members).Error; err != nil {
+	var members []*model.Member
+	if err := db.Where("project_id = ?", pid).Preload("ShuffleLogDetail").Preload("Tags").Find(&members).Error; err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
@@ -36,43 +36,31 @@ func (s *ShuffleService) Shuffle(pid, gcount, mcount int) ([]model.ShuffleLogDet
 		return nil, errors.New("シャッフル対象者リストの抽出で問題が発生しました")
 	}
 
-	// TODO: タグを考慮してシャッフルする
-	// 一人目のタグの人を除外
-	// 二人目を決める
-	// 以下ループ
-	// 不足がある場合、以下をループする
-	// 既にアサイン済みのIDは除いて最小回数の人たちを取得
-	// タグを考慮してシャッフルしてappendする
+	result := groupingMember(targetMemberList, gcount, mcount)
 
 	d := []model.ShuffleLogDetail{}
-	group := 1
-	counter := 0
-	for k := range targetMemberList {
-		detail := model.ShuffleLogDetail{
-			Group:    group,
-			MemberID: targetMemberList[k],
-		}
-		d = append(d, detail)
-
-		counter++
-		if counter == mcount {
-			group++
-			counter = 0
+	for k, v := range result {
+		for _, v := range v {
+			detail := model.ShuffleLogDetail{
+				Group:    k,
+				MemberID: v.ID,
+			}
+			d = append(d, detail)
 		}
 	}
 
 	return d, nil
 }
 
-func makeTargetMemberList(members []model.Member, length int) []int {
-	// シャッフルログからメンバーIDとシャッフル回数のマップを作る
-	memberCountMap := map[int]int{}
+func makeTargetMemberList(members []*model.Member, length int) []*model.Member {
+	// シャッフルログからメンバーとシャッフル回数のマップを作る
+	memberCountMap := map[*model.Member]int{}
 	for _, m := range members {
-		memberCountMap[m.ID] = len(m.ShuffleLogDetail)
+		memberCountMap[m] = len(m.ShuffleLogDetail)
 	}
 
-	// 回数とメンバーIDのマップの配列を作る
-	countMembersMap := map[int][]int{}
+	// 回数とメンバーのマップの配列を作る
+	countMembersMap := map[int][]*model.Member{}
 	for user, count := range memberCountMap {
 		v, _ := countMembersMap[count]
 		countMembersMap[count] = append(v, user)
@@ -86,7 +74,7 @@ func makeTargetMemberList(members []model.Member, length int) []int {
 	sort.Ints(keys)
 
 	// 回数の少ない人から抽出していって、今回の対象者リストを作る
-	var targetMemberList []int
+	var targetMemberList []*model.Member
 	for _, key := range keys {
 		if len(targetMemberList) >= length {
 			break
@@ -106,10 +94,70 @@ func makeTargetMemberList(members []model.Member, length int) []int {
 }
 
 // スライスをシャッフルして返す
-func shuffleSlice(data []int) {
+func shuffleSlice(data []*model.Member) {
 	n := len(data)
 	for i := n - 1; i >= 0; i-- {
 		j := rand.Intn(i + 1)
 		data[i], data[j] = data[j], data[i]
 	}
+}
+
+// TODO: 全体的に汚い
+func groupingMember(members []*model.Member, gcount, mcount int) map[int][]model.Member {
+	result := map[int][]model.Member{}
+
+	for i := 1; i <= gcount; i++ {
+		fmt.Println("gcountの")
+		fmt.Println(i)
+		var m []model.Member
+		var tags []model.Tag
+
+		for i := 1; len(m) < mcount; i++ {
+			fmt.Println("mcountの")
+			fmt.Println(i)
+			var target *model.Member
+			// 対象を取り出す(popするけどcontinueの可能性もあるので、membersを更新しない)
+			target, _ = pop(members)
+
+			// タグ重複がないかチェック
+			targetTags := target.Tags
+			var b bool
+			for _, v := range targetTags {
+				b = contains(tags, v)
+				if b == true {
+					break
+				}
+			}
+
+			// ループしきってないかつタグ重複の時はcontinue
+			if i <= len(members) && b == true {
+				continue
+			}
+
+			// membersを更新するためのpop
+			target, members = pop(members)
+			for _, v := range targetTags {
+				tags = append(tags, v)
+			}
+			m = append(m, *target)
+		}
+
+		result[i] = m
+	}
+	return result
+}
+
+func pop(slice []*model.Member) (*model.Member, []*model.Member) {
+	ans := slice[len(slice)-1]
+	slice = slice[:len(slice)-1]
+	return ans, slice
+}
+
+func contains(s []model.Tag, e model.Tag) bool {
+	for _, v := range s {
+		if e.ID == v.ID {
+			return true
+		}
+	}
+	return false
 }
