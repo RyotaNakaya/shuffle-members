603a7fd40ad097317fcdef1fb3fce0397e0342d8
Ryota Nakaya <0709nakaya@gmail.com>
2020-05-23 21:51:24 +0900

    ユニークインデックス修正

--- internal/model/member.go
+++ internal/model/member.go
@@ -8,8 +8,8 @@ import (
 type Member struct {
 	ID               int    `gorm:"primary_key; AUTO_INCREMENT"`
 	ProjectID        int    `sql:"index"`
-	Name             string `gorm:"unique; not null; size:50;"`
-	Email            string `gorm:"type:varchar(100);unique_index"`
+	Name             string `gorm:"not null; size:50;"`
+	Email            string `gorm:"type:varchar(100);"`
 	Tags             []Tag  `gorm:"many2many:member_tags;association_autoupdate:false"`
 	ShuffleLogDetail []ShuffleLogDetail
 
